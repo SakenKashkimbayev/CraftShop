0ae1ca7d6dda152ea66f2e6563e9ea660d1fd960
Saken <sakenkaskimbaev@gmail.com>
2024-03-25 18:27:21 +0500

    1

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from shop.views import index, category, new_product
+from shop.views import new_product, CategoryListView, ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('', index),
+    # path('', index),
+    path('', CategoryListView.as_view()),
     path('new/', new_product),
-    path('<int:category_id>/', category),
+    path('<int:category_id>/', ProductListView.as_view()),
+    path('<int:category_id>/<int:pk>/', ProductDetailView.as_view()),
 ]
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,26 +1,64 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.views import View
+from django.views.generic import ListView, DetailView
 
 from shop.forms import AddProductForms
 from shop.models import Category, Product
 
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'shop/index.html'
+    context_object_name = 'category'
+    # def get(self, request):
+    #     c = Category.objects.all()
+    #     context = {
+    #         'category': c
+    #     }
+    #     return HttpResponse(render(request, 'shop/index.html', context))
 
-def index(request):
-    c = Category.objects.all()
-    context = {
-        'category': c
-    }
-    return HttpResponse(render(request, 'shop/index.html', context))
 
-def category(request, category_id):
-    c = Category.objects.get(id=category_id)
-    p = Product.objects.filter(category_id=c.id)
-    context = {
-        'category': c,
-        'product': p
-    }
-    return HttpResponse(render(request, 'shop/category.html', context))
+# def index(request):
+#     c = Category.objects.all()
+#     context = {
+#         'category': c
+#     }
+#     return HttpResponse(render(request, 'shop/index.html', context))
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shop/category.html'
+    context_object_name = 'product'
+
+    def get_queryset(self):
+        # Получаем значение параметра маршрута 'category_id'
+        category_id = self.kwargs.get('category_id')
+        # Фильтруем продукты по заданной категории
+        queryset = Product.objects.filter(category__id=category_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Получаем значение параметра маршрута 'category_id'
+        category_id = self.kwargs.get('category_id')
+        # Добавляем объект категории в контекст
+        context['category'] = Category.objects.get(id=category_id)
+        return context
+# def category(request, category_id):
+#     c = Category.objects.get(id=category_id)
+#     p = Product.objects.filter(category_id=c.id)
+#     context = {
+#         'category': c,
+#         'product': p
+#     }
+#     return HttpResponse(render(request, 'shop/category.html', context))
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'shop/product_detail.html'
+    context_object_name = 'product'
+
 
 def new_product(request):
     if request.method == 'POST':
