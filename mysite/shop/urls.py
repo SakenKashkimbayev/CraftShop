be1defb7b8d30deaee5ab68cd72b6fab224a7a0d
Saken <sakenkaskimbaev@gmail.com>
2024-03-26 13:00:06 +0500

    1

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from shop.views import new_product, CategoryListView, ProductListView, ProductDetailView
+from shop import views
+from shop.views import CategoryListView, ProductListView, ProductDetailView, NewProduct
 
 urlpatterns = [
     # path('', index),
-    path('', CategoryListView.as_view()),
-    path('new/', new_product),
+    path('', CategoryListView.as_view(), name='index'),
+    path('new/', NewProduct.as_view()),
+    path('test/', views.test_view),
     path('<int:category_id>/', ProductListView.as_view()),
     path('<int:category_id>/<int:pk>/', ProductDetailView.as_view()),
 ]
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from shop.forms import AddProductForms
 from shop.models import Category, Product
@@ -59,71 +60,24 @@ class ProductDetailView(DetailView):
     template_name = 'shop/product_detail.html'
     context_object_name = 'product'
 
+class NewProduct(CreateView):
+    model = Product
+    template_name = 'shop/new_product.html'
+    success_url = reverse_lazy('index')
+    form_class = AddProductForms
 
-def new_product(request):
-    if request.method == 'POST':
-        form = AddProductForms(request.POST)
-
-        if form.is_valid():
-            print(form.cleaned_data)
-            form.save()
-        else:
-            return HttpResponse('ErrorCreating')
-    context = {
-        'form':AddProductForms()
-    }
-    return render(request, 'shop/new_product.html', context=context)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def index(request):
-#     c = Category.objects.all()
-#     # response = (
-#     #     f'<ul>'
-#     #     f"{ ''.join([f'<li>{c1.name}</li>' for c1 in c])}"
-#     #     f'</ul>'
-#     #
-#     # )
-#     # return HttpResponse('<h1>Добро пожаловать в наш магазин</h1>'
-#     #                     f'В нашем магазине есть следующие категорий товаров: {response}')
+def test_view(request):
+    return render(request, 'shop/test_view.html')
+# def new_product(request):
+#     if request.method == 'POST':
+#         form = AddProductForms(request.POST)
 #
-#     # template = loader.get_template('shop/index.html')
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             form.save()
+#         else:
+#             return HttpResponse('ErrorCreating')
 #     context = {
-#         'category': c
+#         'form':AddProductForms()
 #     }
-#     # return HttpResponse(template.render(context, request))
-#     return HttpResponse(render(request, 'shop/index.html', context))
-#
-# # def category(request):
-# #     category_id = request.GET.get('id', 1)
-# #     c = Category.objects.get(id=category_id)
-# #     return HttpResponse(f'<h1> id: {c.id}. {c.name} </h1>'
-# #                         f'<p> {c.description} </p>')
-# def category(request, category_id):
-#     c = Category.objects.get(id=category_id)
-#     p = Product.objects.filter(category_id=c.id)
-#     response = (
-#         f'<ul>'
-#         f"{''.join([f'<li>{p1.name}</li>' for p1 in p])}"
-#         f'</ul>'
-#
-#     )
-#     return HttpResponse(f'<h1> В категории {c.name} есть товары:</h1>'
-#                         f'{response}')
+#     return render(request, 'shop/new_product.html', context=context)
